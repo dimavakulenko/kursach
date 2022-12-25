76be19bd428b669a1e0f2bac1ebdc8405863cf57
dmitriyvakulenko <denver@trafficstars.com>
2022-12-25 13:40:59 +0300

    cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.database import database
 from app.config import config
@@ -10,6 +11,20 @@ app = FastAPI(
     title=config.APP_NAME
 )
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(executor.router)
 app.include_router(customer.router)
